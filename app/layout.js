3747d111a6e686969007fc7743e481e7fa545a72
celinaLind <clind323@gmail.com>
2024-08-12 18:10:16 -0700

    add favicon imports and header font links to the root file

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,15 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@picocss/pico/css/pico.min.css";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab)
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
