5e8b996002d2aa3982bb710bea60ba30e717f444
celinaLind <clind323@gmail.com>
2024-08-15 21:25:31 -0700

    update formatting/styling of creator profile modal

--- app/components/CreatorProfile.js
+++ app/components/CreatorProfile.js
@@ -1,16 +1,9 @@
 import { useState } from "react";
 import SocialMedia from "./SocialMedia";
+import Link from "next/link";
+import Image from "next/image";
 
-// return (
-//         <div>
-//         <button onClick={openModal}>Add Creator</button>
-//         {isModalOpen && (
-//             <dialog open>
-
-
-export default function CreatorProfile({ creatorData }) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
+export default function CreatorProfile({ creatorData, isModalOpen, setIsModalOpen }) {
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
 
@@ -19,24 +12,25 @@ export default function CreatorProfile({ creatorData }) {
             <button onClick={openModal} className="primaryBtn">Profile<span>-&gt;</span></button>
             {isModalOpen && (
                 <dialog open>
-                    <article>
+                    <article className="modal-styles">
                     <header>
                             <a href="#close" aria-label="Close" className="close" onClick={closeModal}></a>
                             {/* <h3>{creatorData.name}</h3> */}
                         </header>
                         <div className="grid">
-                            <div>
-                                <h2>{creatorData.name}<br />{creatorData.pronouns}</h2>
-                                <h6>{creatorData.brand}</h6>
-                                <image src="@/public/assets/codioful-site-background.jpg" width={200} height={200}></image>
-                                {/* <SocialMedia usernames={creatorData.usernames}></SocialMedia> */}
+                            <div className='center-column'>
+                                <h2 className="playfair-display-headers text-center">{creatorData.name}<br />{creatorData.pronouns}</h2>
+                                <h6 className="text-center">{creatorData.brand}</h6>
+                                <Image src="/assets/codioful-site-background.jpg" width={200} height={200}/>
+                                <SocialMedia instagramUser={creatorData.usernames.instagram} discordChannel={creatorData.usernames.discord}></SocialMedia>
                             </div>
                             <div>
-                                <h3>About</h3>
+                                <h3 className="playfair-display-headers">About</h3>
                                 <p>{creatorData.description}</p>
                                 <div className="grid">
                                     <button className="primaryBtn">DELETE</button>
-                                    <button className="primaryBtn">EDIT</button>
+                                    <Link href="/form?type=Edit" passHref legacyBehavior><button className="primaryBtn">EDIT</button></Link>
+                                    
                                 </div>
                             </div>
                         </div>
