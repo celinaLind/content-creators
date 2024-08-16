45f7ffb02b1f55b6d35916bd4ce2ac88278fa0e6
celinaLind <clind323@gmail.com>
2024-08-15 21:24:02 -0700

    update/fix errors with linking/creation of social media icons

--- app/components/SocialMedia.js
+++ app/components/SocialMedia.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState, useEffect } from 'react'
+import { faDiscord, faInstagram, faLinkedin, faTiktok, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 // Create a SocialMedia component that will display social media links with the ability to distinguish which link is which
-export default function SocialMedia({ linkedInUser, discordUser, discordChannel, instagramUser, email, youtubeUser, tiktokUser, twitchUser }) {
+export default function SocialMedia({linkedInUser, tiktokUser, discordChannel, instagramUser, youtubeUser, twitchUser}) {
+
     return (
         // Add social media links as icons for linkedin, github, instagram, youtube, tiktok, and email
         <div className='social-media'>
-           {linkedInUser && <a className="btn-primary" href={`https://www.linkedin.com/in/${linkedInUser}`} target='_blank' rel='noreferrer'>
-                <FontAwesomeIcon icon="fa-brands fa-linkedin" />
-            </a>}
-            {discordUser && <a className="btn-primary" href={`https://discordapp.com/users/${discordUser}`} target='_blank' rel='noreferrer'>
-                <FontAwesomeIcon icon="fa-brands fa-discord" />
+           { linkedInUser && <a className="btn-primary" href={`https://www.linkedin.com/in/${linkedInUser}`} target='_blank' rel='noreferrer'>
+                <FontAwesomeIcon icon={faLinkedin} />
             </a>}
             {discordChannel && <a className="btn-primary" href={`https://discordapp.com/invite/${discordChannel}`} target='_blank' rel='noreferrer'>
-                <FontAwesomeIcon icon="fa-brands fa-discord" />
-            </a>}
-            {email && <a className="btn-primary" href={`mailto:${email}`} target='_blank' rel='noreferrer'>
-                <FontAwesomeIcon icon={['fab', 'google']} />
+                <FontAwesomeIcon icon={faDiscord} />
             </a>}
             {instagramUser && <a className="btn-primary" href={`https://www.instagram.com/${instagramUser}`} target='_blank' rel='noreferrer'>
-                <FontAwesomeIcon icon={['fab', 'instagram']} />
+                <FontAwesomeIcon icon={faInstagram} />
             </a>}
             {youtubeUser && <a className="btn-primary" href={`https://www.youtube.com/channel/${youtubeUser}`} target='_blank' rel='noreferrer'>
-                <FontAwesomeIcon icon={['fab', 'youtube']} />
+                <FontAwesomeIcon icon={faYoutube} />
             </a>}
             {tiktokUser && <a className="btn-primary" href={`https://www.tiktok.com/@${tiktokUser}`} target='_blank' rel='noreferrer'>
-                <FontAwesomeIcon icon={['fab', 'tiktok']} />
+                <FontAwesomeIcon icon={faTiktok} />
             </a>}
             {twitchUser && <a className="btn-primary" href={`https://www.twitch.tv/${twitchUser}`} target='_blank' rel='noreferrer'>
                 <FontAwesomeIcon icon={['fab', 'twitch']} />
