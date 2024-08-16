8d520b82860602361ee40566670c907294243d83
celinaLind <clind323@gmail.com>
2024-08-15 22:39:56 -0700

    add delete creator function and update paramaters for SocialMedia component

--- app/components/CreatorProfile.js
+++ app/components/CreatorProfile.js
@@ -2,10 +2,27 @@ import { useState } from "react";
 import SocialMedia from "./SocialMedia";
 import Link from "next/link";
 import Image from "next/image";
+import { createClient } from "@/utils/supabase/client";
 
 export default function CreatorProfile({ creatorData, isModalOpen, setIsModalOpen }) {
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
+
+    const DeleteCreator = async (creatorId) => {
+        const supabase = createClient();
+        const { error } = await supabase
+            .from('creators')
+            .delete()
+            .eq('id', creatorId);
+
+        if (error) {
+            console.error('Error deleting creator:', error);
+        } else {
+            console.log('Creator deleted successfully');
+            closeModal();
+            // You might want to add some logic here to refresh the page or update the state
+        }
+    };
 
     return (
         <div>
@@ -22,14 +39,14 @@ export default function CreatorProfile({ creatorData, isModalOpen, setIsModalOpe
                                 <h2 className="playfair-display-headers text-center">{creatorData.name}<br />{creatorData.pronouns}</h2>
                                 <h6 className="text-center">{creatorData.brand}</h6>
                                 <Image src="/assets/codioful-site-background.jpg" width={200} height={200}/>
-                                <SocialMedia instagramUser={creatorData.usernames.instagram} discordChannel={creatorData.usernames.discord}></SocialMedia>
+                                <SocialMedia linkedInUser={creatorData.linkedIn} tiktokUser={creatorData.tiktok} twitchUser={creatorData.twitch} youtubeUser={creatorData.youtube} instagramUser={creatorData.instagram} discordChannel={creatorData.discord}></SocialMedia>
                             </div>
                             <div>
                                 <h3 className="playfair-display-headers">About</h3>
                                 <p>{creatorData.description}</p>
                                 <div className="grid">
-                                    <button className="primaryBtn">DELETE</button>
-                                    <Link href="/form?type=Edit" passHref legacyBehavior><button className="primaryBtn">EDIT</button></Link>
+                                    <button className="primaryBtn" onClick={() => DeleteCreator(creatorData.id)}>DELETE</button>
+                                    <Link href={`/form?type=Edit&creatorData=${encodeURIComponent(JSON.stringify(creatorData))}`} passHref legacyBehavior><button className="primaryBtn">EDIT</button></Link>
                                     
                                 </div>
                             </div>
